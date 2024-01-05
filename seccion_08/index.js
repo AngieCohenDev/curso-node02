7871a75de046631f2f882ab5c4b10ecb03fb4251
Angie Cohen <angiedev2001@gmail.com>
2024-01-05 19:56:21 -0300

    Servidor con express

--- seccion_08/index.js
+++ seccion_08/index.js
@@ -1,6 +1,17 @@
-const http = require('http')
+const express = require('express');
+const fs = require('fs');
 
-http.createServer((request, response) => {
-    response.write('Hola mundo desde el servidor de node');
-    response.end();
-}).listen(8080);
+const server = express();
+const home = fs.readFileSync("./index.html");
+const about = fs.readFileSync("./about.html");
+
+server.get('/', (req, res)=>{
+    res.write(home);
+})
+server.get('/about', (req, res)=>{
+    res.write(about);
+})
+
+server.listen(8080, () => {
+    console.log('El servidor esta corriendo en el puerto 8080');
+})
